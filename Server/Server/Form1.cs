31d5764c3d65afcdd0689f63e2e32bab03be27f3
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-10-15 12:28:50 +0200

    Revert "serverside connectie deel 1"
    This reverts commit e77356df7b077582d5be1348e5ed36ae99718612.

--- Server/Server/Form1.cs
+++ Server/Server/Form1.cs
@@ -8,16 +8,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using SharedCode;
 
 
 namespace Server
 {
     public partial class Server : Form
     {
-        public IPManagement management = new IPManagement();
-        public ClientHandler handler;
-
         public Server()
         {
             InitializeComponent();
@@ -25,7 +21,7 @@ namespace Server
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.handler = new ClientHandler(management.ReturnIpAndPort());
+
         }
 
         private void FolderButton_Click(object sender, EventArgs e)
